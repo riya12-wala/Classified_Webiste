40f9b0b8f85ffdd2cff2e892b8197367d803ba23
riya12-wala <riyawala02@gmail.com>
2025-08-06 12:39:30 +0530

    cors changed

--- node/index.js
+++ node/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use((cors({  origin: process.env.FRONTEND_URL,credentials:true})));
+app.use((cors({  origin: process.env.CLIENT_URL,credentials:true})));
 
 const PORT = process.env.PORT || 8000
 
